2783ed865dd7c2bf63c8884aa3016be92d2505bf
Too.Shigeyuki <shigeyuki.to@techno-creative.co.jp>
2024-09-18 08:31:50 +0900

    プロパティ開発用

---
+++ Models/Interfaces/IDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Convenience.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Convenience.Models.Interfaces {
+    public interface IDbContext {
+        private const string ConfigrationFileName = "appsettings.json"; 
+        private const string KeyWordInAppConfig = "ConvenienceContext";
+
+        protected static ConvenienceContext DbOpen() {
+            //DBコンテクスト用接続子読み込み
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile(ConfigrationFileName, optional: true, reloadOnChange: true)
+                .Build();
+            //DBコンテクスト作成
+            var contextOptions = new DbContextOptionsBuilder<ConvenienceContext>()
+                .UseNpgsql(configuration[KeyWordInAppConfig])
+                .Options;
+            return (new ConvenienceContext(contextOptions));
+        }
+    }
+}
