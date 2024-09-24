1da77ce57c16b9c2ff332fbfd7844537f7a7ec7c
Too.Shigeyuki <shigeyuki.to@techno-creative.co.jp>
2024-09-24 16:44:29 +0900

    コメント追加

--- Models/Interfaces/IChumonService.cs
+++ Models/Interfaces/IChumonService.cs
@@ -8,25 +8,31 @@ namespace Convenience.Models.Interfaces {
 
     public interface IChumonService {
         /// <summary>
-        /// * 注文クラス用オブジェクト変数
+        /// 注文クラス用オブジェクト変数
         /// </summary>
         public IChumon chumon { get; set; }
+
+        /// <summary>
+        /// 注文キービューモデル初期設定
+        /// </summary>
+        /// <returns>ChumonKeysViewModel 注文キービューモデル</returns>
+        public Task<ChumonKeysViewModel> SetChumonKeysViewModel();
 
         /// <summary>
         /// 注文セッティング
         /// </summary>
         /// <param name="inShiireSakiId">仕入先コード（画面より）</param>
         /// <param name="inChumonDate">注文日付（画面より）</param>
-        /// <returns>注文viewモデル</returns>
-        public Task<ChumonViewModel> ChumonSetting(string inShiireSakiId, DateOnly inChumonDate);
+        /// <returns>ChumonKeysViewModel 注文明細ビューモデル</returns>
+        public Task<ChumonViewModel> ChumonSetting(ChumonKeysViewModel inChumonKeysViewModel);
 
         /// <summary>
         /// 注文データをDBに書き込む
         /// </summary>
         /// <param name="inChumonJisseki">Postされた注文実績</param>
-        /// <returns></returns>
+        /// <returns>ChumonViewModel 注文明細ビューモデル</returns>
         /// <exception cref="Exception">排他制御の例外が起きたらスローする</exception>
-        public Task<(ChumonJisseki, int, bool, ErrDef)> ChumonCommit(ChumonJisseki inChumonJisseki);
+        public Task<ChumonViewModel> ChumonCommit(ChumonViewModel inChumonViewModel);
     }
 
 }
